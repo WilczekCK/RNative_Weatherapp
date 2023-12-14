53773109e425841b59cc846a1524e6385a385bb6
Pawel <wilkuwdr2008@gmail.com>
2023-12-14 23:31:26 +0100

    display 100vh background image (in future depends on api)

--- App.jsx
+++ App.jsx
@@ -1,25 +1,57 @@
 import React from 'react';
 import {
+  ImageBackground,
   SafeAreaView,
   ScrollView,
   StyleSheet,
   Text,
   View,
+  Dimensions
 } from 'react-native';
 
+
+
 function App() {
+  const [weatherBackground, setWeatherBackground] = React.useState('unset');
+
+  function detectWeatherBackground(){
+    // TODO: Ajax check from api.
+    setWeatherBackground(require('./assets/jpgs/sunny.jpg'));
+  }
+
+  React.useEffect(() => {
+    detectWeatherBackground();
+  }, [])
+
   return (
     <SafeAreaView>
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         >
-        <View>
-          <Text>Hello world!</Text>
+        <View style={styles.rootContainer}>
+          <ImageBackground
+            source={weatherBackground}
+            resizeMode="cover"
+            style={styles.rootImageBackground}
+            >
+              <Text>Hello world!</Text>
+          </ImageBackground>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
 
+let ScreenHeight = Dimensions.get("window").height;
+const styles = StyleSheet.create({
+  rootContainer: {
+    minHeight: ScreenHeight
+  },
+  rootImageBackground: {
+    flex: 1,
+    paddingTop: 20,
+    paddingBottom: 20,
+  }
+})
 
 export default App;
