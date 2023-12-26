0e664bcbd63de06d3aff247c491716a1517c75ee
Pawel <wilkuwdr2008@gmail.com>
2023-12-26 19:03:01 +0100

    workaround for dynamic component icon require

--- components/Icon.jsx
+++ components/Icon.jsx
@@ -1,16 +1,77 @@
 import React from 'react';
 import { Image, View, Text } from 'react-native';  
-import Sun from "../assets/icons/sun.svg";
 
 function Icon(props) {
     const {name} = props; 
-    console.log(name);
     
     return (
         <View className="icon__container">
-            <Sun width={120} height={40} />
+            <Image 
+                source={ICONS_IMAGES[name].uri}
+                alt={ICONS_IMAGES[name].alt}
+                resizeMode="contain"
+            />
         </View>
     );
 };
+
+const ICONS_IMAGES = {
+    settings: {
+        altName: 'settings',
+        uri: require(`../assets/icons/settings.png`)
+    },
+    arrowRightBold: {
+        altName: 'arrow-right-bold',
+        uri: require(`../assets/icons/arrow-right-bold.png`)
+    },
+    arrowLeftBold: {
+        altName: 'arrow-left-bold',
+        uri: require(`../assets/icons/arrow-left-bold.png`)
+    },
+    cloudSun: {
+        altName: 'cloud-sun',
+        uri: require(`../assets/icons/cloud-sun.png`)
+    },
+    cloud: {
+        altName: 'cloud',
+        uri: require(`../assets/icons/cloud.png`)
+    },
+    humidity: {
+        altName: 'humidity',
+        uri: require(`../assets/icons/humidity.png`)
+    },
+    pressure: {
+        altName: 'pressure',
+        uri: require(`../assets/icons/pressure.png`)
+    },
+    rainfall: {
+        altName: 'rainfall',
+        uri: require(`../assets/icons/rainfall.png`)
+    },
+    search: {
+        altName: 'search',
+        uri: require(`../assets/icons/search.png`)
+    },
+    snowing: {
+        altName: 'snowing',
+        uri: require(`../assets/icons/snowing.png`)
+    },
+    sunHot: {
+        altName: 'sun-hot',
+        uri: require(`../assets/icons/sun-hot.png`)
+    },
+    sun: {
+        altName: 'sun',
+        uri: require(`../assets/icons/sun.png`)
+    },
+    wind: {
+        altName: 'wind',
+        uri: require(`../assets/icons/wind.png`)
+    },
+    windy: {
+        altName: 'windy',
+        uri: require(`../assets/icons/windy.png`)
+    },
+}
 
 export default Icon;
