10d9b6c6661799f9bfe6408e90296bf32b84403d
Pawel <wilkuwdr2008@gmail.com>
2024-01-09 23:04:14 +0100

    prettier of home component

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,115 +1,82 @@
-import { Text, Button, View, StyleSheet } from 'react-native';  
+import {Text, Button, View, StyleSheet} from 'react-native';
 import Header from '../components/Header';
 import Heading from '../components/Heading';
 import Icon from '../components/Icon';
 import WeatherCard from '../components/WeatherCard';
 
 const Home = ({navigation}) => {
-    return (
-        <>
-          <Header
-            actualRoute="Home"
-            navigation={navigation}
-          />
-          <View style={styles.container}>
-            <Heading 
-              heading="h2"
-            >
-              Kielce
+  return (
+    <>
+      <Header actualRoute="Home" navigation={navigation} />
+      <View style={styles.container}>
+        <Heading heading="h2">Kielce</Heading>
+
+        <Heading heading="h1">20°C</Heading>
+
+        <Heading heading="h4" color="#E1E1E1">
+          Cloudy sky
+        </Heading>
+
+        <View style={styles.weatherVisualStatus}>
+          <Icon name="cloudSun" width="80" height="80" />
+        </View>
+
+        <View style={styles.sunriseSunsetContainer}>
+          <View style={styles.sunriseContainer}>
+            <Heading heading="h4" color="#E1E1E1">
+              SUNRISE
             </Heading>
 
-            <Heading 
-              heading="h1"
-            >
-              20°C
-            </Heading>
-
-            <Heading 
-              heading="h4"
-              color="#E1E1E1"
-            >
-              Cloudy sky
-            </Heading>
-
-            <View style={styles.weatherVisualStatus}>
-              <Icon
-                name="cloudSun"
-                width="80"
-                height="80"
-              />
-            </View>
-
-            <View style={styles.sunriseSunsetContainer}>
-              <View style={styles.sunriseContainer}>
-                <Heading 
-                  heading="h4"
-                  color="#E1E1E1"
-                >
-                  SUNRISE
-                </Heading>
-
-                <Heading 
-                  heading="h2"
-                >
-                  6:11AM
-                </Heading>
-              </View>
-              <View style={styles.sunsetContainer}>
-                <Heading 
-                  heading="h4"
-                  color="#E1E1E1"
-                >
-                  SUNSET
-                </Heading>
-                <Heading 
-                  heading="h2"
-                >
-                  9:54PM
-                </Heading>
-              </View>
-            </View>
-
-            <View style={styles.cardsContainer}>
-              <WeatherCard />
-              <WeatherCard />
-              <WeatherCard />
-              <WeatherCard />
-              <WeatherCard />
-            </View>
+            <Heading heading="h2">6:11AM</Heading>
           </View>
-        </>
-    );
+          <View style={styles.sunsetContainer}>
+            <Heading heading="h4" color="#E1E1E1">
+              SUNSET
+            </Heading>
+            <Heading heading="h2">9:54PM</Heading>
+          </View>
+        </View>
+
+        <View style={styles.cardsContainer}>
+          <WeatherCard />
+          <WeatherCard />
+          <WeatherCard />
+          <WeatherCard />
+          <WeatherCard />
+        </View>
+      </View>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex', 
+    display: 'flex',
     flexDirection: 'column',
-    justifyContent:'center',
+    justifyContent: 'center',
     alignItems: 'center',
   },
   weatherVisualStatus: {
-    paddingTop:     10,
-    paddingBottom:  10
+    paddingTop: 10,
+    paddingBottom: 10,
   },
   sunriseSunsetContainer: {
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   sunriseContainer: {
-    width: '65%'
+    width: '65%',
   },
   sunsetContainer: {
-    width: '65%'
+    width: '65%',
   },
   cardsContainer: {
     display: 'flex',
     flexDirection: 'row',
     gap: 8,
-    marginTop: 25
-  }
-})
-
+    marginTop: 25,
+  },
+});
 
 export default Home;
