9b5981405ed09a6e5c868094a1bf3a4e1ad4aae3
Pawel <wilkuwdr2008@gmail.com>
2024-01-14 20:17:21 +0100

    created "empty" component for new item

---
+++ components/WeatherDaysList.jsx
@@ -0,0 +1,16 @@
+import React, {Children} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import Heading from './Heading';
+import Icon from './Icon';
+
+function WeatherDaysList() {
+  return (
+    <View>
+      Hello world
+    </View>
+  );
+}
+
+
+
+export default WeatherDaysList;
